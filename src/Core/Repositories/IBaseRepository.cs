7da34f3b5a9a9b0f8e1580a8e5bf55cf9f6a2ea3
hrshuv0 <h.r.shuvo9@gmail.com>
2023-03-06 22:06:29 +0600

    Add base repository

--- src/Core/Repositories/IBaseRepository.cs
+++ src/Core/Repositories/IBaseRepository.cs
@@ -14,21 +14,29 @@ public interface IBaseRepository<TEntity, TKey> where TEntity : IBaseEntity<TKey
         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
         bool disableTracking = true);
 
-    (IList<TResult> Items, int Total, int TotalFilter) Get<TResult>(Expression<Func<TEntity, TResult>> selector, 
+    Task<(IList<TResult> Items, int Total, int TotalFilter)> GetAsync<TResult>(Expression<Func<TEntity, TResult>> selector, 
         Expression<Func<TEntity, bool>>? predicate= null,
         Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
         int pageIndex = 1, int pageSize = 10,
         bool disableTracking = true);
 
-    TResult GetFirstOrDefault<TResult>(Expression<Func<TEntity, TResult>> selector,
+    Task<TResult> GetFirstOrDefaultAsync<TResult>(Expression<Func<TEntity, TResult>> selector,
         Expression<Func<TEntity, bool>>? predicate=null,
         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, 
         bool disableTracking = true);
 
-    TEntity GetById(TKey id);
-    int GetCount(Expression<Func<TEntity, bool>>? predicate = null);
-    bool IsExists(Expression<Func<TEntity, bool>>? predicate);
+    Task<TEntity> GetByIdAsync(TKey id);
+    Task<int> GetCountAsync(Expression<Func<TEntity, bool>>? predicate = null);
+    Task<bool> IsExistsAsync(Expression<Func<TEntity, bool>> predicate);
+    Task AddAsync(TEntity entity);
+    Task AddRangeAsync(IList<TEntity> entities);
+    Task UpdateAsync(TEntity entity);
+    Task UpdateRangeAsync(IList<TEntity> entities);
+    Task DeleteAsync(TKey id);
+    Task DeleteAsync(TEntity entity);
+    Task DeleteAsync(Expression<Func<TEntity, bool>> predicate);
+    Task DeleteRangeAsync(IList<TEntity> entities);
 
 
     #endregion
@@ -40,7 +48,12 @@ public interface IBaseRepository<TEntity, TKey> where TEntity : IBaseEntity<TKey
 
     #region SQL
 
+    IList<TEntity> ExecuteSqlQuery(string sql, params object[] parameters);
+    int ExecuteSqlCommand(string sql, params object[] parameters);
+    IList<dynamic> GetFromSql(string sql, Dictionary<string, object> parameters, bool isStoredProcedure = false);
 
+    (IList<TEntity> Items, int Total, int TotalFilter) GetFromSql(string sql,
+        IList<(string Key, object Value, bool IsOut)> parameters, bool isStoredProcedure = true);
 
     #endregion
 }
