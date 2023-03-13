40cf9a479e220ea265873f53bb5403c8fa9b0294
hrshuv0 <h.r.shuvo9@gmail.com>
2023-03-13 23:01:36 +0600

    SPA - add form validation

--- src/client/src/app/account/register/register.component.ts
+++ src/client/src/app/account/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -10,16 +10,26 @@ export class RegisterComponent implements OnInit{
 
   registerForm! : FormGroup;
 
-  ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      username: new FormControl(),
-      email: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl()
-
-    });
+  constructor(private fb: FormBuilder) {
   }
 
+  ngOnInit(): void {
+
+    this.createRegisterForm();
+  }
+
+  createRegisterForm(){
+    this.registerForm = this.fb.group({
+      // username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      confirmPassword: new FormControl('', Validators.required)
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(g: AbstractControl){
+    return g.get('password')?.value === g.get('confirmPassword')?.value ? null : {'mismatch':true};
+  }
 
   register(){
     console.log(this.registerForm.value);
