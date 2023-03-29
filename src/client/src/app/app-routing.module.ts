50061cfee55b063551ed135cbed6aab8249563db
hrshuv0 <h.r.shuvo9@gmail.com>
2023-03-29 19:46:29 +0600

    SPA -  update routing module

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home/home.component";
-import { CategoryListComponent } from "./category/category-list/category-list.component";
-import { AuthGuard } from "./_guards/auth.guard";
-import { CategoryEditComponent } from "./category/category-edit/category-edit.component";
-
 
 const routes: Routes = [
   { path : '', component: HomeComponent},
-  { path : 'category', component: CategoryListComponent, canActivate:[AuthGuard]},
-  { path : 'category/edit/:id', component: CategoryEditComponent, canActivate:[AuthGuard]},
+  { path : 'category', loadChildren: () => import('./category/category.module').then(mod => mod.CategoryModule)},
 
   { path : 'account', loadChildren : () => import('./account/account.module').then(mod => mod.AccountModule)},
   { path : '**', redirectTo: '', pathMatch: 'full'}
--- src/client/src/app/category/category.module.ts
+++ src/client/src/app/category/category.module.ts
@@ -2,23 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryEditComponent } from './category-edit/category-edit.component';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterModule, Routes } from "@angular/router";
 import { FormsModule } from "@angular/forms";
+import { CategoryCreateComponent } from './category-create/category-create.component';
+import { AuthGuard } from "../_guards/auth.guard";
 
 
+const routes: Routes = [
+  { path : '', component: CategoryListComponent, canActivate:[AuthGuard]},
+  { path : 'create', component: CategoryCreateComponent, canActivate:[AuthGuard]},
+  { path : 'edit/:id', component: CategoryEditComponent, canActivate:[AuthGuard]},
+
+];
 
 @NgModule({
   declarations: [
     CategoryListComponent,
-    CategoryEditComponent
+    CategoryEditComponent,
+    CategoryCreateComponent
   ],
   exports: [
-    CategoryListComponent
+    CategoryListComponent,
+    RouterModule
   ],
   imports: [
     CommonModule,
     RouterLink,
-    FormsModule
+    FormsModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class CategoryModule { }
