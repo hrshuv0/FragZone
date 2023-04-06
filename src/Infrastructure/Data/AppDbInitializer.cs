26bf4e29192e34339a7c2e3b47e772ec973f79e5
hrshuv0 <h.r.shuvo9@gmail.com>
2023-04-07 01:11:07 +0600

    Add seed data - publisher, game

--- src/Infrastructure/Data/AppDbInitializer.cs
+++ src/Infrastructure/Data/AppDbInitializer.cs
@@ -12,12 +12,36 @@ public class AppDbInitializer
         {
             if (!context.Categories!.Any())
             {
-                var categoryData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/categories.json");
+                var categoryData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/category.json");
                 var categories = JsonSerializer.Deserialize<List<Category>>(categoryData);
 
                 foreach (var item in categories!)
                 {
                     context.Categories!.Add(item);
+                }
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Publishers!.Any())
+            {
+                var publisherData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/publisher.json");
+                var publishers = JsonSerializer.Deserialize<List<Publisher>>(publisherData);
+                
+                foreach (var item in publishers!)
+                {
+                    context.Publishers!.Add(item);
+                }
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Games!.Any())
+            {
+                var gameData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/game.json");
+                var games = JsonSerializer.Deserialize<List<Game>>(gameData);
+                
+                foreach (var item in games!)
+                {
+                    context.Games!.Add(item);
                 }
                 await context.SaveChangesAsync();
             }
