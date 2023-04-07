4e1ff377d07e0a7db61803b3aa358dbd9fd5f291
hrshuv0 <h.r.shuvo9@gmail.com>
2023-04-07 17:14:55 +0600

    Add helper class - enum to dictionary

---
+++ src/API/Controllers/CommonHelperController.cs
@@ -0,0 +1,16 @@
+﻿using Core.Enums;
+using Infrastructure.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class CommonHelperController : BaseApiController
+{
+    [HttpGet("game-mode")]
+    public IActionResult GetGameModes()
+    {
+        var gameModes =  FragHelper.LoadEnumToDictionary<Mode>();
+
+        return Ok(gameModes);
+    }
+}
---
+++ src/Infrastructure/Utility/FragHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Infrastructure.Utility;
+
+public static class FragHelper
+{
+    public static Dictionary<string, int> LoadEnumToDictionary<TEnum>() where TEnum : Enum
+    {
+        var dict = new Dictionary<string, int>();
+        var enumType = typeof(TEnum);
+        var enumValues = Enum.GetValues(enumType);
+        
+        foreach (var enumValue in enumValues)
+        {
+            var enumName = Enum.GetName(enumType, enumValue);
+            var enumDescription = enumType.GetField(enumName!)!.GetCustomAttribute<DescriptionAttribute>()?.Description;
+            if (enumDescription != null)
+            {
+                dict.Add(enumDescription, (int)enumValue);
+            }
+        }
+        return dict;
+    }
+}
