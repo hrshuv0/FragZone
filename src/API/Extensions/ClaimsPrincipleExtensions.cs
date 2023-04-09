2860dfbeeabcf08787b73c505230b8db6e64ffaf
hrshuv0 <h.r.shuvo9@gmail.com>
2023-04-09 23:50:14 +0600

    Add Claims principle extension

---
+++ src/API/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+
+namespace API.Extensions;
+
+public static class ClaimsPrincipleExtensions
+{
+    public static string GetUserId(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.NameIdentifier)?.Value!;
+    }
+    
+    public static string GetUserName(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.Name)?.Value!;
+    }
+    
+    public static string GetUserEmail(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.Email)?.Value!;
+    }
+    
+    public static string GetUserRole(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.Role)?.Value!;
+    }
+    
+    
+    
+    
+    
+}
